0a3878d930f8f4d63a42ee9303d6cae8b84629dc
Sunil <krlinus@yahoo.com>
2016-10-14 20:40:52 -0700

    investigating "import this" in python

--- variables/cpp11/test1.cpp
+++ variables/cpp11/test1.cpp
@@ -1,5 +1,19 @@
+#include <vector>
+#include <iostream>
+template<typename T>
+void ilist(std::initializer_list<T> vals) {
+	//for(auto p=vals.begin(); p!= vals.end(); ++p) 
+	for(auto p : vals)
+		std::cout << p << std::endl;
+}
+
 int main() {
 	char c{7};
+	std::vector<int> n{2,3,4,5,6};
+	for(auto m : n) {
+		std::cout << m << std::endl;
+	}
+	ilist({"Hello","How","do","you","do"});
 	return 0;
 }
 

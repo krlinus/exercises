55d125830279ca3824f5864690d5bc35219e3b41
kidambis <kidambis@google.com>
2016-10-25 16:57:56 -0700

    fixing improper merge

--- problems/sol_14.c
+++ problems/sol_14.c
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 /*
-12. Given an array of integers possibly containing negative numbers, find the
+14. Given an array of integers possibly containing negative numbers, find the
 contiguous subarray with the maximum sum
 */
 #include <stdio.h>
@@ -36,44 +35,4 @@ int main() {
   return 0;
 }
 
-=======
 
-/*
-12. Given a non-negative integer num, repeatedly add all its digits until the result has only 1 digit
-*/
-
-#include <stdio.h>
-#include <string.h>
-
-void to_str(int n, char *str) {
-  int i=0;
-  while(n) {
-    int t = n % 10;
-    str[i] = t + '0';
-    i++;
-    n /= 10;
-  }
-  str[i]='\0';
-}
-
-int warp(char *strnum) {
-  int l;
-  int res=0;
-  while((l=strlen(strnum)) > 1) {
-    res=0;
-    for(int i=0;i<l;++i) {
-      res += strnum[i] - '0';
-    }
-    to_str(res,strnum);
-  }
-  return res;
-}
-
-int main() {
-  char numbuf[1000];
-  scanf("%s",numbuf);
-  printf("%d\n",warp(numbuf));
-  return 0;
-}
- 
->>>>>>> b87a14e2fafaf81bc6c34f48704ca50d99f18917
